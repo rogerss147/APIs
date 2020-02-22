a245a6a4c3e71288997395512ffcbb3a0e41bd20
Roger Souza <rogersouzasilva@hotmail.com>
2020-02-22 18:26:25 -0300

    Commit

---
+++ cadastro-api/src/test/java/br/com/rogerss147/cadastroapi/CadastroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rogerss147.cadastroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
